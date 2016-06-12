41bfc457b7ed20afd201ca3525fbd5dc1c82f4d6
SpikeSrb <spasojevic.ivan@gmail.com>
2016-06-12 19:54:38 +0200

    2nd commit, added some basics on basepage

--- automation_core/basepage.py
+++ automation_core/basepage.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from automation_core.confparser import ConfigParser
+from selenium.webdriver.common.by import By
 
 
 class BasePage:
     page_name = ""
     parser = ConfigParser()
 
-    def __init__(self, driver: parser.selected_driver):
+    def __init__(self, driver: webdriver.Firefox):
         self.driver = driver
 
-    def get_element(self, by, element_path):
+    def get_element(self, by: str, element_path: str):
         """
         gets webelement by any given way:
         xpath, id, css, name...so on...
@@ -18,4 +19,8 @@ class BasePage:
         :param element_path:
         :return:
         """
-        return self.driver.find_element(by, element_path)
+        by.lower()
+        if by == "xpath":
+            return self.driver.find_element_by_xpath(element_path)
+        if by == "id":
+            return self.driver.find_element_by_id(element_path)
