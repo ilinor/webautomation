873a354701f2b12fa83864a547f29d86d94ffeb7
ilinor <spasojevic.ivan@gmail.com>
2016-08-25 21:34:42 +0200

    Update basepage.py

--- automation_core/basepage.py
+++ automation_core/basepage.py
@@ -2,17 +2,22 @@
 import logging
 from selenium import webdriver
 from selenium.common import exceptions
-
 from automation_core.confparser import ConfigParser
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class BasePage:
     page_name = ""
     parser = ConfigParser()
 
-    def __init__(self, driver: webdriver.Firefox):
-        self.driver = driver
+    def __init__(self):
+        if self.parser.selected_driver == "Chrome":
+            self.driver = webdriver.Chrome
+        elif self.parser.selected_driver == "Firefox":
+            self.driver = webdriver.Firefox
+        elif self.parser.selected_driver == "Ie":
+            self.driver = webdriver.Ie
 
     def get_element(self, by: str, element_path: str):
         """
@@ -25,15 +30,15 @@ class BasePage:
         by = by.lower()
         try:
             if by == "xpath":
-                return self.driver.find_element_by_xpath(element_path)
+                return self.driver.find_element(By.XPATH, element_path)
             if by == "id":
-                return self.driver.find_element_by_id(element_path)
+                return self.driver.find_element(By.ID, element_path)
             if by == "name":
-                return self.driver.find_element_by_name(element_path)
+                return self.driver.find_element(By.NAME, element_path)
             if by == "class":
-                return self.driver.find_element_by_class(element_path)
+                return self.driver.find_element(By.CLASS_NAME, element_path)
             if by == "css":
-                return self.driver.find_element_by_css(element_path)
+                return self.driver.find_element(By.CLASS_NAME, element_path)
 
         except (exceptions.WebDriverException, AssertionError) as ex:
             self.take_screenshot()
@@ -41,7 +46,6 @@ class BasePage:
             print(type(ex).__name__)
             print(ex.args)
             self.fail("Caught an exception")
-
 
     def get_elements(self, by: str, elements: str):
         """
@@ -53,15 +57,16 @@ class BasePage:
         by = by.lower()
         try:
             if by == "xpath":
-                return self.driver.find_elements_by_xpath(elements)
+                return self.driver.find_elements(By.XPATH, element_path)
             if by == "id":
-                return self.driver.find_elements_by_id(elements)
+                return self.driver.find_elements(By.ID, element_path)
             if by == "name":
-                return self.driver.find_elements_by_name(elements)
+                return self.driver.find_elements(By.NAME, element_path)
             if by == "class":
-                return self.driver.find_elements_by_class(elements)
+                return self.driver.find_elements(By.CLASS_NAME, element_path)
             if by == "css":
-                return self.driver.find_elements_by_css(elements)
+                return self.driver.find_elements(By.CSS_SELECTOR, element_path)
+
 
         except (exceptions.WebDriverException, AssertionError) as ex:
             self.take_screenshot()
@@ -69,3 +74,11 @@ class BasePage:
             print(type(ex).__name__)
             print(ex.args)
             self.fail("Caught an exception")
+
+    def click(self, element):
+        actions = ActionChains(self.driver)
+        actions.click(element).perform()
+    
+    def double_click(self, element):
+        actions = ActionChains(self.driver)
+        actions.double_click(element).perform()
